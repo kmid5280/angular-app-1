bba578d0385ffe51bbd418838217ece0b0bcc623
Kyle Middleton <kylemid321@gmail.com>
2019-05-14 14:26:05 -0600

    set style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -9,7 +9,8 @@ import { CoursesService } from './courses.service';
                 <td [attr.colspan]="colSpan"></td>
             </tr>
         </table>
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button 
+            [style.backgroundColor]="isActive? 'blue' : 'white'">Save</button>
         `
 })
 export class CoursesComponent {
