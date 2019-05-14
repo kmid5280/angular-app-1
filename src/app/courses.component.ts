8adcfc34778add4e94add51baf32761e71522eba
Kyle Middleton <kylemid321@gmail.com>
2019-05-14 13:47:57 -0600

    add colspan attribute to template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,21 +4,21 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ getTitle() }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
         `
 })
 export class CoursesComponent {
     title = "List of courses";
     courses
+    colSpan = 2
 
-    constructor() {
-        let service = new CoursesService();
+    constructor(service: CoursesService) {
         this.courses = service.getCourses();
+
     }
     getTitle() {
         return this.title;
