dbc46a6a96df576839b0da1e90af77149cef95ce
Kyle Middleton <kylemid321@gmail.com>
2019-05-14 12:46:58 -0600

    implement list of courses using ngFor

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent {
     title = "List of courses";
+    courses = ['1','2','3']
     getTitle() {
         return this.title;
     }
