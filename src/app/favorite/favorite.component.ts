a903a0f22343b4df40f8d8afd5be8de124b1f7d9
Kyle Middleton <kylemid321@gmail.com>
2019-05-15 16:34:19 -0600

    initial commit

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css'],
+})
+export class FavoriteComponent implements OnInit {
+  @Input('is-favorite') isSelected: boolean;
+  status: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.status = !this.status
+  }
+}
