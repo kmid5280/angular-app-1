34370341e5d5b8540f238105b30e22c436c09deb
Kyle Middleton <kylemid321@gmail.com>
2019-05-18 19:15:14 -0600

    change onBlur to make all characters in input lowercase

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,15 +1,16 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  @HostListener('focus') onFocus() {
-    console.log('on focus')
-  }
+
+  constructor(private el: ElementRef) { }
+
   @HostListener('blur')  onBlur() {
-    console.log('on blur')
+    let value = this.el.nativeElement.value;
+    this.el.nativeElement.value = value.toLowerCase();
   }
-  constructor() { }
+  
 
 }
